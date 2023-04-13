48f5e7d7f847cd32a9bd5e5a98a6bb7598d54a51
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2023-04-13 14:56:00 +0300

    Task 2 v1
    Task 2 first version
    Complete:
    Customer delete
    Customer edit
    Customer add
    
    To do:
    Change icons for delete and edit
    Create add training functionality on customer list page + date picker component
    Delete training

--- personal-training/src/components/CustomerList.js
+++ personal-training/src/components/CustomerList.js
@@ -2,27 +2,43 @@ import React, {useState,useEffect} from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
-import { useRef } from 'react';
 import Button from '@mui/material/Button';
 import { Snackbar } from '@mui/material';
+import AddCustomer from './AddCustomer';
 
-export default function Customerapp(){
+import EditCustomer from './EditC'
+
+ function Customerapp(){
     const[customers,setCustomers]= useState([]);
-   // const gridRef = useRef();
     useEffect(() => {
         getCustomers()},[]);
-//const[open,setOpen]=useState(false);
+    const[open,setOpen]=useState(false);
+
 
     const[columnDefs]=useState([
-        //{field:'id',sortable:true,filtering:true},
         {field:'firstname',sortable:true,filtering:true, filter: 'agTextColumnFilter',width:150},
-        {field:'lastname',sortable:true,filtering:true, filter: 'agTextColumnFilter'},
+        {field:'lastname',sortable:true,filtering:true, filter: 'agTextColumnFilter',width:150},
         {field:'streetaddress',sortable:true,filtering:true, filter: 'agTextColumnFilter'},
         {field:'postcode',sortable:true,filtering:true, filter: 'agTextColumnFilter',width:150},
         {field:'city',sortable:true,filtering:true, filter: 'agTextColumnFilter',width:150},
-        {field:'email',sortable:true,filtering:true, filter: 'agTextColumnFilter'},
-        {field:'phone',sortable:true,filtering:true, filter: 'agTextColumnFilter'}
+        {field:'email',sortable:true,filtering:true, filter: 'agTextColumnFilter',width:150},
+        {field:'phone',sortable:true,filtering:true, filter: 'agTextColumnFilter',width:150},
+        {cellRenderer:params=>
+            <EditCustomer updateCustomer={updateCustomer} params={params.data}/>
+                , width:100, filtering:false,sortable:false},   
+        {cellRenderer: params=>
+            <Button 
+                size='small' 
+                color='error'
+                onClick={()=> deleteCustomer(params)}
+            >
+                Delete
+                </Button>
+                , width:120, filtering:false,sortable:false},
+         
     ])
+
+
 
     const getCustomers = () =>{
         fetch('http://traineeapp.azurewebsites.net/api/customers')
@@ -37,18 +53,63 @@ export default function Customerapp(){
         .catch(err=> console.error(err))
     }
 
+
+    const saveCustomer=(customer)=>{
+        fetch('http://traineeapp.azurewebsites.net/api/customers',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(customer)
+        }
+        )
+        .then(res=> getCustomers())
+        .catch(err=>console.error(err))
+    }
+
+    const updateCustomer=(customer,link)=>{
+        fetch(link,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(customer)
+        }
+        )
+        .then(res=> getCustomers())
+        .catch(err=>console.error(err))
+    }
+
+    const deleteCustomer =(link)=>{
+       // console.log(link);
+        if(window.confirm('Are you sure?')){
+        fetch(link.data.links[0].href,{method:'DELETE'})
+        .then(response=>{
+            if(response.ok){
+                setOpen(true);
+                getCustomers();
+            }
+            else{
+                alert('Something went wrong in deletion');
+            }
+        })
+        .catch(err=> console.log(err))
+    }}
+
     return(
           <>
           <h3 style={{margin:'auto', textAlign:'left', width:'90%'}}>
             Customer Information
           </h3>
+          <div style={{width:'90%'}}>
+          <AddCustomer saveCustomer={saveCustomer}/>
+          
+          </div>
           <hr style={{ width:'90%'}}></hr>
         <div className='ag-theme-material'
         style={{width:'90%', height: 600, margin:'auto'}}
         >
             <AgGridReact
-              //  ref={gridRef}
-              //  onGridReady={ params => gridRef.current = params.api }
                 rowSelection="single"
                 rowData={customers}
                 animateRows= {true}
@@ -57,7 +118,14 @@ export default function Customerapp(){
                 paginationPageSize={10}
             />
         </div>
+        <Snackbar
+                    open={open}
+                    message="Customer deleted successfully"
+                    autoHideDuration={3000}
+                    onClose={()=>setOpen(false)}
+                />
+        
         
         </>
     );
-}
+}export default Customerapp;
---
+++ personal-training/src/components/EditC.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+  
+export default function EditCustomer(props){
+    const [open, setOpen] = React.useState(false);
+    const [customer, setCustomer]= React.useState({
+        firstname:'', 
+        lastname:'', 
+        streetaddress:'', 
+        postcode:'', 
+        city:'', 
+        email:'', 
+        phone:''
+    })
+
+    const handleClickOpen = () => {
+     // console.log(props.params)
+      setCustomer({
+        firstname:props.params.firstname, 
+        lastname:props.params.lastname,
+        streetaddress:props.params.streetaddress,
+        postcode:props.params.postcode,
+        city:props.params.city,
+        email:props.params.email,
+        phone:props.params.phone
+      })
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+      const handleInputChange = (event) =>{
+        setCustomer({...customer,
+          [event.target.name]:event.target.value
+        })
+      }
+
+      const updateCustomer = ()=>{
+        props.updateCustomer(customer,props.params.links[0].href);
+        handleClose();
+      }
+
+      
+    return(
+    <div>
+      <Button size="small" onClick={handleClickOpen}>
+        Edit
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Customer</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="firstname"
+                    value={customer.firstname}
+                    onChange={e=>handleInputChange(e)}
+                    label="Firstname"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="lastname"
+                    value={customer.lastname}
+                    onChange={e=>handleInputChange(e)}
+                    label="Lastname"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="streetaddress"
+                    value={customer.streetaddress}
+                    onChange={e=>handleInputChange(e)}
+                    label="Street Address"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="postcode"
+                    value={customer.postcode}
+                    onChange={e=>handleInputChange(e)}
+                    label="Post Code"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="city"
+                    value={customer.city}
+                    onChange={e=>handleInputChange(e)}
+                    label="City"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="email"
+                    value={customer.email}
+                    onChange={e=>handleInputChange(e)}
+                    label="Email"
+                    fullWidth
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="phone"
+                    value={customer.phone}
+                    onChange={e=>handleInputChange(e)}
+                    label="Phone"
+                    fullWidth
+                />
+            </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={updateCustomer}>Save</Button>
+        </DialogActions>
+      </Dialog>
+        </div>
+    )
+
+}
